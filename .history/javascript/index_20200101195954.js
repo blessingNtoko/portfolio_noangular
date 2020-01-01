22b0cbb5685fa0f770a21d5c73da333bada20b36
Blessing Ntoko <bntoko@gmail.com>
2020-01-01 20:00:14 +0200

    minpr

---
+++ .history/javascript/index_20200101195954.js
@@ -0,0 +1,24 @@
+const contactForm = document.querySelector('#contactForm');
+const formName = document.querySelector('#formName');
+const formEmail = document.querySelector('#formEmail');
+const formMess = document.querySelector('#formMess');
+
+contactForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const formContact = {
+        name: formName.value,
+        email: formEmail.value,
+        message: formMess.value
+    };
+
+    console.log('[Contact Form]>>>>', JSON.stringify(formContact));
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:4177');
+    xhr.send(JSON.stringify(formContact));
+
+    contactForm.reset();
+}
